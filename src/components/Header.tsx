6ea79c8347a4bdb41d3573bd4cf60b5cbb3127e3
Adrian <adrian.tubio@outlook.com>
2024-04-22 21:32:04 +0800

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const Header: Component = () => {
+  return (
+    <header class="flex w-screen bg-neutral-700 px-[30px] h-20 items-center py-[20px]">
+      <h1 class="font-bold text-5xl tracking-wide text-white">eyd</h1>
+    </header>
+  );
+};
+
+export default Header;
