a21ba8fd521ec66f7ec98dc284c506c8cbc68e3d
Adrian <adrian.tubio@outlook.com>
2024-04-25 22:01:26 +0800

    remove outline and stop creating newline on enter

--- src/components/ContentLine.tsx
+++ src/components/ContentLine.tsx
@@ -9,13 +9,20 @@ const ContentLine = (props: ContentLineProps) => {
   const { lineCount, content, editable } = props;
   let contentElement!: HTMLSpanElement;
 
+
+  const disableEnter = (e: KeyboardEvent) => {
+    if (e.key == 'Enter') {
+      e.preventDefault();
+    }
+  }
+
   return (
     <div class="flex w-screen text-white px-14 py-2 hover:bg-zinc-800" onClick={() => { contentElement.focus() }}>
       <div class="pr-8">
         <h1>{lineCount}</h1>
       </div>
       <div class="w-100">
-        <span contentEditable={editable} class="w-100" ref={contentElement}>{content}</span>
+        <span contentEditable={editable} class="w-100 focus:outline-none height-full inline-block" ref={contentElement} onKeyPress={disableEnter}>{content}</span>
       </div>
     </div>
   );
